8a81fee02d94ec994271339ba5a60fb561a64634
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-15 13:41:07 -0500

    feat(developer-registration): add developer registration logic and state management.

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,38 @@
 <script setup>
 
+import {ref} from "vue";
+import {Developer} from "./greetings/domain/model/developer.entity.js";
+import DeveloperRegistration from "./greetings/presentation/components/developer-registration.vue";
+
+const firstName = ref('');
+const lastName = ref('');
+const developerCount = ref(0);
+const hasRegistered = ref(false);
+
+function updateRegisteredDeveloperInfo(developer) {
+  firstName.value = developer.firstName;
+  lastName.value = developer.lastName;
+  hasRegistered.value = true;
+  console.log('Developer registered: ', developer);
+  updateDeveloperCount(developer);
+}
+
+function resetRegisteredDeveloperInfo() {
+  firstName.value = '';
+  lastName.value = '';
+  hasRegistered.value = false;
+  console.log('Registration deferred');
+}
+
+function updateDeveloperCount(developer) {
+  const dev = new Developer(developer.firstName, developer.lastName);
+  if (dev.fullName !== 'Unknown') developerCount.value++;
+}
 </script>
 
 <template>
-
+  <h1>Hello Vue Developer Application</h1>
+  <developer-registration
+    @developer-registered="updateRegisteredDeveloperInfo"
+    @registration-deferred="resetRegisteredDeveloperInfo"/>
 </template>
